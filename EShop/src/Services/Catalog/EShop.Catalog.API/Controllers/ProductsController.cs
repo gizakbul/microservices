31de4f4ee5178819c5505a721a37f8d806fd62a7
Gizem Akbulut <57941313+gizakbul@users.noreply.github.com>
2025-01-16 09:10:06 +0300

    Solution folder structure completed and Catalog service implementation started.  Talking about DDD and Clean Architecture. Implementing MediatR

---
+++ EShop/src/Services/Catalog/EShop.Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using EShop.Catalog.Application.Features.Products.Queries;
+using EShop.Catalog.Application.Services;
+using EShop.Catalog.Domain.Aggregates;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> GetProducts()
+        {
+            //var productService = new ProductService();
+            //var products = productService.GetProducts();
+
+            var request = new GetAllProductsQueryRequest();
+            var products = _mediator.Send(request);
+
+            var result = Ok(products);
+            return Task.FromResult<IActionResult>(result);
+        }
+    }
+}
